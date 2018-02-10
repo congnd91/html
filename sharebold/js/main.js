3ac4b600eb706d7bb224a523b0010d07c84d80d0
congnd91 <congnd91@gmail.com>
2018-02-10 13:59:48 +0700

    mess

---
+++ sharebold/js/main.js
@@ -0,0 +1,71 @@
+(function ($) {
+    $(document).on('ready', function () {
+
+        var grid = $('.grid').isotope({
+            itemSelector: '.grid-item',
+        });
+
+        $('.menu-icon').on('click', function () {
+            $('body').toggleClass("open-menu");
+            setTimeout(scrollToTop, 0);
+        });
+        $('.menu-res li.menu-item-has-children').on('click', function (event) {
+            event.stopPropagation();
+            var submenu = $(this).find(" > ul");
+            if ($(submenu).is(":visible")) {
+                $(submenu).slideUp();
+                $(this).removeClass("open-submenu-active");
+            } else {
+                $(submenu).slideDown();
+                $(this).addClass("open-submenu-active");
+            }
+        });
+
+        $('.menu-res li.menu-item-has-children > a').on('click', function () {
+            //  return false;
+        });
+
+
+
+
+        "use strict";
+        /**Preload**/
+        $('#page-loader').delay(800).fadeOut(600, function () {
+            $('body').fadeIn();
+        });
+        /** slider**/
+        var owl_contest = $('.owl-contest')
+        $(owl_contest).owlCarousel({
+            loop: true,
+            margin: 0,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 6000,
+            items: 1,
+            animateOut: 'fadeOutDown',
+            animateIn: 'fadeInDown',
+        });
+
+        $('.open-test-right').click(function () {
+            $('body').addClass("show-test-right");
+        });
+        $('.close-test-right').click(function () {
+            $('body').removeClass("show-test-right");
+        });
+
+        //show test-button
+        //$('.box-answer').click(function () {
+        //    $('body').addClass("show-test-bottom");
+        //});
+        //$('.close-test-bottom').click(function () {
+        //    $('body').removeClass("show-test-bottom");
+        //});
+
+        //menu user
+
+        $('.user-info').click(function () {
+            $('.user-panel').toggleClass("show-user-menu");
+        });
+
+    });
+})(jQuery);
