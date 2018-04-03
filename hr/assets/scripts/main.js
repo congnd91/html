41623f893ff077a9a2d4e7afccc2aa046cafed0a
congnd91 <congnd91@gmail.com>
2018-04-03 17:05:16 +0700

    mess

--- hr/assets/scripts/main.js
+++ hr/assets/scripts/main.js
@@ -15,7 +15,13 @@
         $('.menu-icon').on('click', function () {
             $('body').toggleClass("open-menu");
         });
-        /**register-form**/
+
+
+        /**-form**/
         $("#register-form").validate();
+
+        $('.answer').on('click', function () {
+            $(this).toggleClass("current");
+        });
     });
 })(jQuery);
