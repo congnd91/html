136f197e4a1b380862d6f9b04fa46db4dec2c8c1
congnd91 <congnd91@gmail.com>
2018-04-11 16:37:27 +0700

    mess

--- cr/js/main.js
+++ cr/js/main.js
@@ -14,6 +14,38 @@
 
 
 
+        var owl_intro = $('.owl-intro');
+        if ($(owl_intro).length) {
+            $(owl_intro).owlCarousel({
+                loop: true,
+                margin: 0,
+                nav: true,
+                autoplay: true,
+                navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+                responsive: {
+
+                    400: {
+                        items: 1
+                    },
+
+                    480: {
+                        items: 2
+                    },
+
+                    768: {
+                        items: 3
+                    },
+                    992: {
+                        items: 4
+                    },
+                    1200: {
+                        items: 5
+                    }
+                }
+
+            });
+        }
+
         var owl_home = $('.owl-home');
         if ($(owl_home).length) {
             $(owl_home).owlCarousel({
@@ -21,13 +53,11 @@
                 margin: 0,
                 nav: false,
                 autoplay: true,
-                center: true,
                 navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
                 items: 1,
 
             });
         }
-
 
 
 
