8b85cd42422b827024d337edb11349e120c92f20
congnd91 <congnd91@gmail.com>
2018-04-23 15:21:04 +0700

    company

--- db/js/main.js
+++ db/js/main.js
@@ -1,32 +1,55 @@
 (function ($) {
+
     $(document).on('ready', function () {
-        $('#page-loader').delay(800).fadeOut(600, function () {
-            $('body').fadeIn();
-        });
+
+
+
+        var db = new Object();
+
+        db.preLoad = function () {
+            $('#page-loader').delay(800).fadeOut(600, function () {
+                $('body').fadeIn();
+            });
+
+        }
+
+        db.menuAccordion = function () {
+
+            $('.acc-caption').on('click', function () {
+                var content = $(this).next(".acc-content");
+
+                if ($(content).is(":visible")) {
+                    $(this).removeClass("open");
+                    $(content).stop(true, true).slideUp();
+                } else {
+                    $(content).stop(true, true).slideDown();
+                    $(this).addClass("open");
+                }
+
+            });
+
+        }
+
+
+
+
+
+
+
 
 
         /**Menu**/
         $('.menu-icon').on('click', function () {
             $('body').toggleClass("open-menu");
-            setTimeout(scrollToTop, 0);
+
         });
 
 
 
 
-        var owl_home = $('.owl-home');
-        if ($(owl_home).length) {
-            $(owl_home).owlCarousel({
-                loop: true,
-                margin: 0,
-                nav: false,
-                autoplay: true,
-                center: true,
-                navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-                items: 1,
 
-            });
-        }
+        db.preLoad();
+        db.menuAccordion();
 
 
 
