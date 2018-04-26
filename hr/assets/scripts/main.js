b0a22a97a5196171aabea887badddfdac8ddc0a1
congnd91 <congnd91@gmail.com>
2018-04-26 13:31:45 +0700

    c

--- hr/assets/scripts/main.js
+++ hr/assets/scripts/main.js
@@ -16,14 +16,24 @@
             $('body').toggleClass("open-menu");
         });
 
+        var inPageOffest = $('.control-step2-fixed.inpage').offset().top - $(window).height();
+        $(window).scroll(function () {
+            var scrollTop = $(this).scrollTop() - $('control-step2-fixed.inpage').outerHeight() - 100;
+            if (scrollTop >= inPageOffest) {
+                $('body').addClass("no-fixed");
+            } else {
+                $('body').removeClass("no-fixed");
+            }
+        });
 
-     
         /**-form**/
         $("#register-form").validate();
 
         $('.answer').on('click', function () {
             $(this).toggleClass("current");
         });
+
+
 
 
 
