240a859b85fc49f458d855cf2de73ec4373da42e
congnd91 <congnd91@gmail.com>
2018-06-07 17:11:39 +0700

    ct5

--- monamie/js/main.js
+++ monamie/js/main.js
@@ -6,6 +6,37 @@
                 $('body').fadeIn();
             });
         }
+        db.sliderHome = function () {
+            var owl_home = $('.owl-home');
+            if ($(owl_home).length) {
+                $(owl_home).owlCarousel({
+                    loop: true,
+                    margin: 0,
+                    nav: true,
+                    autoplay: true,
+                    margin: 0,
+                    items: 1,
+                    animateOut: 'fadeOut',
+                    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+                });
+            }
+        }
+
+        db.sliderFade = function () {
+            var owl_fade = $('.owl-fade');
+            if ($(owl_fade).length) {
+                $(owl_fade).owlCarousel({
+                    loop: true,
+                    margin: 0,
+                    nav: false,
+                    autoplay: true,
+                    margin: 0,
+                    items: 1,
+                    animateOut: 'fadeOut',
+
+                });
+            }
+        }
         db.sliderPopular = function () {
             var owl_popular = $('.owl-popular');
             if ($(owl_popular).length) {
@@ -14,6 +45,7 @@
                     margin: 0,
                     nav: true,
                     autoplay: false,
+                    margin: 30,
                     responsive: {
                         0: {
                             items: 1,
@@ -22,30 +54,33 @@
                             items: 2,
                         },
                         768: {
-                            items: 3,
+                            items: 2,
                         },
                         991: {
-                            items: 4,
+                            items: 3,
                         },
                         1199: {
-                            items: 5,
+                            items: 4,
                         }
                     }
                 });
             }
         }
 
+
+
         db.niceScroll = function () {
 
             if ($('.md-brand-group-scroll').length) {
                 $('.md-brand-group-scroll').niceScroll({
-
                     autohidemode: 'leave'
                 });
             }
         }
         db.preLoad();
         db.sliderPopular();
+        db.sliderHome();
+        db.sliderFade();
         db.niceScroll();
     });
 })(jQuery);
