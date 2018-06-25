acb947819e9e2dfccb34e24e7f57e08883927288
congnd91 <congnd91@gmail.com>
2018-06-25 17:16:22 +0700

    c t 2

--- test/js/main.js
+++ test/js/main.js
@@ -16,7 +16,7 @@
                     autoplay: false,
                     responsive: {
                         0: {
-                            items: 1,
+                            items: 2,
                         },
                         576: {
                             items: 2,
