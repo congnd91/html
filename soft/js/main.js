34a28be92cd3360a2a068a245a73f131b44d4b70
congnd91 <congnd91@gmail.com>
2018-07-11 21:52:42 +0700

    aaa

--- soft/js/main.js
+++ soft/js/main.js
@@ -1,4 +1,6 @@
 (function ($) {
+
+    new WOW().init();
     $(document).on('ready', function () {
         var db = new Object();
         db.preLoad = function () {
@@ -6,51 +8,22 @@
                 $('body').fadeIn();
             });
         }
-        db.menuResponsive = function () {
-
-
-
+        db.menu = function () {
             $(window).bind('resize', function () {
                 //    $('.menubar').css("min-height", "500px");
             }).trigger('resize');
-
             if ($(".menubar").length) {
-
-
                 $(".menubar").stick_in_parent();
             }
-
-
+            $('.dropdown-mobile').click(function () {
+                $(this).toggleClass("show");
+            });
 
 
 
 
         }
-
-
-
-
         db.preLoad();
-        db.menuResponsive();
-
-        /*  $(window).scroll(function () {
-      var scrollDistance = $(window).scrollTop();
-
-      // Show/hide menu on scroll
-      //if (scrollDistance >= 850) {
-      //		$('nav').fadeIn("fast");
-      //} else {
-      //		$('nav').fadeOut("fast");
-      //}
-
-      // Assign active class to nav links while scolling
-      $('.section').each(function (i) {
-          if ($(this).position().top <= scrollDistance + 100) {
-              $('.navigation a.active').removeClass('active');
-              $('.navigation a').eq(i).addClass('active');
-          }
-      });
-  }).scroll();*/
-
+        db.menu();
     });
 })(jQuery);
