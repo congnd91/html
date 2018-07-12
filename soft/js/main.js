1a2b807cfe4e5ec755f671f4efe6a3c0e236f81d
congnd91 <congnd91@gmail.com>
2018-07-12 17:00:53 +0700

    ê

--- soft/js/main.js
+++ soft/js/main.js
@@ -1,6 +1,33 @@
 (function ($) {
 
-    new WOW().init();
+    //  new WOW().init(); var $body = $('body');
+    var $body = $('body');
+    var $box = $('.box');
+    for (var i = 0; i < 20; i++) {
+        $box.clone().appendTo($body);
+    }
+
+    // Helper function for add element box list in WOW
+    WOW.prototype.addBox = function (element) {
+        this.boxes.push(element);
+    };
+
+    // Init WOW.js and get instance
+    var wow = new WOW();
+    wow.init();
+
+    // Attach scrollSpy to .wow elements for detect view exit events,
+    // then reset elements and add again for animation
+    $('.wow').on('scrollSpy:exit', function () {
+        $(this).css({
+            'visibility': 'hidden',
+            'animation-name': 'none'
+        }).removeClass('animated');
+        wow.addBox(this);
+    }).scrollSpy();
+
+
+
     $(document).on('ready', function () {
         var db = new Object();
         db.preLoad = function () {
