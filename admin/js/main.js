45f06b6b61da4a5831e36d127e1e2e0378a3fb30
congnd91 <congnd91@gmail.com>
2019-01-29 16:05:10 +0700

    t3

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,6 +1,28 @@
 (function ($) {
     $(document).on('ready', function () {
         var db = new Object();
+
+
+
+        db.sidebarMenu = function () {
+
+            $('.sidebar-menu-item.has-child.active .sidebar-sub-menu').show();
+
+            $('.sidebar-menu-item.has-child >a').on('click', function (e) {
+                var menu = $(this).parent().find(".sidebar-sub-menu");
+                if ($(menu).is(":visible")) {
+                    $(menu).slideUp();
+                    $(this).parent().removeClass("active");
+                } else {
+                    $(menu).slideDown();
+                    $(this).parent().addClass("active");
+                }
+                return false;
+
+            });
+        }
+
+
         db.menuResponsive = function () {
             $('.menu-icon').on('click', function () {
                 var menu = $('.menu-res');
@@ -157,7 +179,7 @@
         }
 
 
-
+        db.sidebarMenu();
         db.menuResponsive();
         db.scrollMenu();
         db.gridTemplate();
