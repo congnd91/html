62639e301caee24ba50e46e327dd2f717d15b331
congnd91 <congnd91@gmail.com>
2019-02-18 08:35:51 +0700

    ss

--- LHB/js/main.js
+++ LHB/js/main.js
@@ -6,6 +6,18 @@
                 $('body').fadeIn();
             });
         }
+        $(window).bind('resize', function () {
+
+
+            $(".menu li a").click(function () {
+                var check = $(".mobile-check")
+                if ($(check).is(":visible")) {} else {
+
+                    $(".menu").stop(true, true).slideUp();
+                }
+            });
+
+        }).trigger('resize');
         db.menuResponsive = function () {
             $('.menu-icon').on('click', function (e) {
                 var menu = $('.menu');
@@ -22,6 +34,21 @@
             }
             if ($('.item-two').length) {
                 $('.item-two').matchHeight();
+            }
+        }
+
+        db.sliderBanner = function () {
+            var owl_banner = $('.owl-banner');
+            if ($(owl_banner).length) {
+                $(owl_banner).owlCarousel({
+                    loop: true,
+                    margin: 0,
+                    mouseDrag: true,
+                    nav: false,
+                    autoplay: true,
+                    items: 1,
+
+                });
             }
         }
         db.sliderOne = function () {
@@ -58,7 +85,7 @@
                     autoplay: true,
                     responsive: {
                         0: {
-                            items: 2,
+
                         },
                         768: {
                             items: 2,
@@ -78,11 +105,25 @@
                 $("#gender").val(gender);
             });
         }
+        db.scroll = function () {
+            $(".menu li a").click(function () {
+                $(".menu li").removeClass("active");
+                var that = this;
+                var id = $(that).attr("data-id");
+                $(that).parent().addClass("active");
+                $('html,body').animate({
+                    scrollTop: $(id).offset().top - 120
+                }, 1000);
+            });
+        }
         db.preLoad();
         db.menuResponsive();
         db.matchHeight();
+        db.sliderBanner();
         db.sliderOne();
         db.sliderTwo();
         db.gender();
+        db.scroll();
+        new WOW().init();
     });
 })(jQuery);
