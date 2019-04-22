564f53d2e08ef7c363a0522b2e54b6ba8f04bc12
congnd91 <congnd91@gmail.com>
2019-04-22 22:56:33 +0700

    message

--- where/js/main.js
+++ where/js/main.js
@@ -63,12 +63,62 @@
                 });
             }
         }
+        db.blogSlider = function () {
+            var owl_blog = $('.owl-blog');
+            if ($(owl_blog).length) {
+                $(owl_blog).owlCarousel({
+                    loop: true,
+                    margin: 0,
+                    //mouseDrag: false,
+                    nav: false,
+                    autoplay: true,
+                    items: 1,
+                    animateOut: 'fadeOut'
+                });
+            }
+        }
+
+
+        db.reviewSlider = function () {
+            var owl_review = $('.owl-rv');
+            if ($(owl_review).length) {
+                $(owl_review).owlCarousel({
+                    loop: true,
+                    margin: 15,
+                    //mouseDrag: false,
+                    nav: true,
+                    autoplay: true,
+                    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+                    responsive: {
+
+                        0: {
+                            items: 5,
+                        },
+
+                        576: {
+                            items: 6,
+                        },
+
+                        768: {
+                            items: 7,
+
+                        },
+                        991: {
+                            items: 8,
+
+                        }
+                    },
+                });
+            }
+        }
 
 
         db.preLoad();
         db.scrollMenu();
         db.menuResponsive();
         db.aboutSlider();
+        db.reviewSlider();
+        db.blogSlider();
 
     });
 })(jQuery);
