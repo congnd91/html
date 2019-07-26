0c5104ebe7c21484500c5ac0e952e4e1eafad578
congnd91 <congnd91@gmail.com>
2019-07-26 17:29:11 +0700

    e

--- demo/js/main.js
+++ demo/js/main.js
@@ -14,6 +14,7 @@
         db.stepSelected = function () {
             $('.step-item .dot').on('click', function () {
                 $(this).parent().toggleClass("active");
+
             });
         }
         db.accodion = function () {
