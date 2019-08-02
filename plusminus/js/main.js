c6e593c7afefd44f5634ca7485e7b796b30cb37c
congnd91 <congnd91@gmail.com>
2019-08-02 15:28:09 +0700

    mess

--- plusminus/js/main.js
+++ plusminus/js/main.js
@@ -2,19 +2,11 @@
     $(document).on('ready', function () {
         var db = new Object();
         db.preLoad = function () {
-            $('#page-loader').delay(800).fadeOut(600, function () {
+            $('.page-preload').delay(800).fadeOut(600, function () {
                 $('body').fadeIn();
             });
         }
-        db.scrollMenu = function () {
-            $(window).scroll(function () {
-                if ($(this).scrollTop() >= 50) {
-                    $('.header').addClass("header-scrolled");
-                } else {
-                    $('.header').removeClass("header-scrolled");
-                }
-            });
-        }
+
         db.menuResponsive = function () {
             $('.menu-icon').on('click', function (e) {
                 e.stopPropagation();
@@ -35,48 +27,93 @@
                 return false;
             });
         }
-        db.partnerSlider = function () {
-            var owl_partner = $('.owl-partner');
-            if ($(owl_partner).length) {
-                $(owl_partner).owlCarousel({
-                    loop: true,
-                    margin: 20,
-                    //mouseDrag: false,
-                    nav: true,
-                    autoplay: true,
-                    responsive: {
-                        0: {
-                            items: 2,
-                        },
-                        576: {
-                            items: 2,
-                        },
-                        768: {
-                            items: 3,
-                        },
-                        991: {
-                            items: 4,
-                        },
-                        1199: {
-                            items: 5,
-                        }
-                    },
+
+
+        db.sliderHay = function () {
+            if ($('.slider-hay').length) {
+                $('.slider-hay').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    fade: false,
+                    dots: true,
                 });
+
             }
+
         }
-        db.testimonialSlider = function () {
-            var owl_testimonial = $('.owl-testimonial');
-            if ($(owl_testimonial).length) {
-                $(owl_testimonial).owlCarousel({
-                    loop: true,
-                    margin: 20,
-                    //mouseDrag: false,
-                    nav: false,
-                    autoplay: true,
-                    items: 1,
+        db.sliderMallistomme = function () {
+            if ($('.mallistomme-slider').length) {
+                $('.mallistomme-slider').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    fade: false,
+                    dots: true,
                 });
+
             }
+
         }
+
+        db.sliderReferensseja = function () {
+            if ($('.slider-referensseja').length) {
+                $('.slider-referensseja').slick({
+
+                    arrows: true,
+                    dots: true,
+
+                    centerMode: true,
+                    centerPadding: '230px',
+                    slidesToShow: 2,
+                    responsive: [
+                        {
+                            breakpoint: 768,
+                            settings: {
+                                arrows: false,
+                                centerMode: true,
+                                centerPadding: '30px',
+                                slidesToShow: 1
+                            }
+    },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                arrows: false,
+                                centerMode: false,
+                                centerPadding: '0px',
+                                slidesToShow: 1
+                            }
+    }]
+
+                });
+
+
+            }
+
+        }
+
+
+
+        db.sliderGrid = function () {
+            if ($('.grid').length) {
+                $('.grid').isotope({
+                    // options
+                    itemSelector: '.grid-item',
+                    layoutMode: 'fitRows'
+                });
+
+            }
+
+        }
+
+
+
+
+
+
+
+
         new WOW({
             offset: 100,
             mobile: true
@@ -96,16 +133,32 @@
                 }, 1000);
             });
         }
+
+
+        db.mallistommeDropdown = function () {
+
+            $(".mallistomme-dropdown").click(function () {
+
+                $(".mallistomme-bar").toggleClass("open");
+
+            });
+        }
+
+
         db.matchHeight = function () {
             if ($('.se-step').length) {
                 $('.se-step').matchHeight();
             }
         };
         db.preLoad();
-        db.scrollMenu();
+
         db.menuResponsive();
-        db.partnerSlider();
-        db.testimonialSlider();
-        db.matchHeight();
+
+        db.sliderHay();
+        db.sliderMallistomme();
+        db.sliderReferensseja();
+
+        db.sliderGrid();
+        db.mallistommeDropdown();
     });
 })(jQuery);
