d027b01bb0aab02160f416deed69cef3f608f783
congnd91 <congnd91@gmail.com>
2019-08-18 17:44:55 +0700

    mess

--- gluwa/js/main.js
+++ gluwa/js/main.js
@@ -11,121 +11,48 @@
                 e.stopPropagation();
                 $('body').toggleClass("open-menu");
             });
-            $('.page').on('click', function () {
+            $('.close-menu').on('click', function () {
                 $('body').removeClass("open-menu");
             });
 
         }
-        db.sliderHay = function () {
-            if ($('.slider-hay').length) {
-                $('.slider-hay').slick({
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: true,
-                    fade: false,
-                    dots: true,
+        db.focus = function () {
+            $(function () {
+                $('.db-txt').focusin(function () {
+                    $(this).parent().addClass('focus');
                 });
-            }
-        }
-        db.sliderMallistomme = function () {
-            if ($('.mallistomme-slider').length) {
-                $('.mallistomme-slider').slick({
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    arrows: true,
-                    fade: false,
-                    dots: true,
+                $('.db-txt').focusout(function () {
+                    $(this).parent().removeClass('focus');
                 });
-            }
-        }
-        db.sliderReferensseja = function () {
-            if ($('.slider-referensseja').length) {
-                $('.slider-referensseja').slick({
-                    arrows: true,
-                    dots: true,
-                    centerMode: true,
-                    centerPadding: '230px',
-                    slidesToShow: 2,
-                    responsive: [
-                        {
-                            breakpoint: 992,
-                            settings: {
-                                arrows: false,
-                                centerMode: false,
-                                centerPadding: '30px',
-                                slidesToShow: 2
-                            }
-    },
-                        {
-                            breakpoint: 768,
-                            settings: {
-                                arrows: false,
-                                centerMode: false,
-                                centerPadding: '0px',
-                                slidesToShow: 1
-                            }
-    }]
-                });
-            }
-        }
-        /*db.sliderGrid = function () {
-            if ($('.grid').length) {
-                $('.grid').isotope({
-                    // options
-                    itemSelector: '.grid-item',
-                    layoutMode: 'fitRows'
-                });
-            }
-        }*/
+            });
 
-        db.toTop = function () {
-            $('.totop').hide();
-            $(window).scroll(function () {
-                if ($(this).scrollTop() >= 50) {
-                    $('.totop').fadeIn();
-                } else {
-                    $('.totop').fadeOut();
+        }
+        db.keyMark = function () {
+
+            var myColors = ["#C2C7CE", "#C9CFD6"];
+
+            $('.hidden-key').find('p').each(function () {
+
+                var $el = $(this),
+                    text = $el.text(),
+                    len = text.length,
+                    coLen = myColors.length,
+                    newCont = '';
+
+                for (var i = 0; i < len; i++) {
+                    newCont += '<span style="background:' + myColors[i % coLen] + '">' + text.charAt(i) + '</span>';
                 }
-            });
-            $(".totop").click(function () {
-                $("html, body").animate({
-                    scrollTop: 0
-                }, 1000);
-            });
-        }
-        db.mallistommeDropdown = function () {
-            $(".mallistomme-dropdown").click(function () {
-                $(".mallistomme-bar").toggleClass("open");
+
+                $el.html(newCont);
+
             });
 
-            $(".mallistomme-dropdown-content a").click(function () {
-                $(".mallistomme-bar").removeClass("open");
-                return false;
-            });
         }
 
-    /*    $('.logo-item').css("min-height", $('.logo-item').width() + "px");
-    $(window).bind('resize', function () {
-        $('.logo-item').css("min-height", $('.logo-item').width() + "px");
-    }).trigger('resize');*/
-
-
-        db.matchHeight = function () {
-          /*  if ($('.logo-item').length) {
-      $('.logo-item').matchHeight();
-  }*/
-        }
         db.preLoad();
+        db.focus();
+        db.keyMark();
         db.menuResponsive();
-        db.sliderHay();
-        db.sliderMallistomme();
-        db.sliderReferensseja();
-        //  db.sliderGrid();
-        db.mallistommeDropdown();
-        db.matchHeight();
-        new WOW({
-            offset: 100,
-            mobile: true
-        }).init()
+
     });
 })(jQuery);
