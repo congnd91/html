1c41545cef57219bebd8a2596e6576acfdc30148
congnd91 <congnd91@gmail.com>
2019-09-17 20:48:23 +0700

    mess

--- pet/js/main.js
+++ pet/js/main.js
@@ -15,7 +15,7 @@
         db.scrollMenu = function () {
             $(window).scroll(function () {
                 if ($(this).scrollTop() >= 50) {
-                    $('.header').addClass("header-scrolled");
+                    $('.header-scroll').addClass("header-scrolled");
                 } else {
                     $('.header').removeClass("header-scrolled");
                 }
@@ -36,6 +36,40 @@
                 });
             }
         }
+
+        db.tipSlider = function () {
+            var owl_tip = $('.owl-tip');
+            if ($(owl_tip).length) {
+                $(owl_tip).owlCarousel({
+                    loop: false,
+                    margin: 30,
+                    mouseDrag: false,
+                    nav: true,
+                    autoplay: false,
+                    items: 1,
+                    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+
+                    responsive: {
+                        0: {
+                            items: 1,
+                        },
+                        576: {
+                            items: 2,
+                        },
+                        768: {
+                            items: 2,
+                        },
+                        991: {
+                            items: 3,
+                        },
+                        1199: {
+                            items: 4,
+                        }
+                    },
+                    //      animateOut: 'fadeOut'
+                });
+            }
+        }
         db.matchHeight = function () {
             if ($('.ls-item ul').length) {
                 $('.ls-item ul').matchHeight();
@@ -44,9 +78,23 @@
                 $('.project-row .project-col').matchHeight();
             }
         }
+        new WOW({
+            offset: 100,
+            mobile: true
+        }).init();
+
+        if ($('#datepicker').length) {
+            $('#datepicker').datepicker({
+                uiLibrary: 'bootstrap4'
+            });
+        }
+
+
+
         db.preLoad();
         db.matchHeight();
         db.friendsSlider();
+        db.tipSlider();
         db.menuResponsive();
         db.scrollMenu();
     });
