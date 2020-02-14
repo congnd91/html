01ec2a9519b588219a2774f41f96bab2f437c8a6
congnd91 <congnd91@gmail.com>
2020-02-14 16:32:28 +0700

    mess

---
+++ recruit/js/main.min.js
@@ -0,0 +1,26 @@
+(function ($) {
+    $(document).on('ready', function () {
+        var db = new Object();
+        db.preLoad = function () {
+            $('#page-loader').delay(800).fadeOut(600, function () {
+                $('body').fadeIn();
+            });
+        }
+        db.menuResponsive = function () {
+            $('.menu-icon').on('click', function (e) {
+                e.stopPropagation();
+                $('body').toggleClass("open-menu");
+            });
+            $('.page').on('click', function () {
+                $('body').removeClass("open-menu");
+            });
+        }
+        db.matchHeight = function () {
+            if ($('.service-item').length) {
+                $('.service-item').matchHeight();
+            }
+        }
+        db.preLoad();
+        db.menuResponsive();
+    });
+})(jQuery);
